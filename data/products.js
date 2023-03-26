81af0db1424a89a8ae0295a36634a5ca98192804
Trung Nguyen <q.trung1201@gmail.com>
2023-03-27 02:03:42 +0700

    update

---
+++ data/products.js
@@ -0,0 +1,253 @@
+var list_products = [{
+    "name": "SamSung Galaxy J4+",
+    "company": "Samsung",
+    "img": "img/products/samsung-galaxy-j4-plus-pink-400x400.jpg",
+    "price": "3.490.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.0', HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "13 MP",
+        "camaraFront": "5 MP",
+        "cpu": "Qualcomm Snapdragon 425 4 nhân 64-bit",
+        "ram": "2 GB",
+        "rom": "16 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3300 mAh"
+    },
+    "masp": "Sam0"
+}, {
+    "name": "Samsung Galaxy A8+ (2018)",
+    "company": "Samsung",
+    "img": "img/products/samsung-galaxy-a8-plus-2018-gold-600x600.jpg",
+    "price": "11.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.500.000"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6', Full HD+",
+        "os": "Android 7.1 (Nougat)",
+        "camara": "16 MP",
+        "camaraFront": "16 MP và 8 MP (2 camera)",
+        "cpu": "Exynos 7885 8 nhân 64-bit",
+        "ram": "6 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3500 mAh, có sạc nhanh"
+    },
+    "masp": "Sam1"
+}, {
+    "name": "iPhone X 256GB Silver",
+    "company": "Apple",
+    "img": "img/products/iphone-x-256gb-silver-400x400.jpg",
+    "price": "31.990.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "giareonline",
+        "value": "27.990.000"
+    },
+    "detail": {
+        "screen": "OLED, 5.8', Super Retina",
+        "os": "iOS 11",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic 6 nhân",
+        "ram": "3 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "2716 mAh, có sạc nhanh"
+    },
+    "masp": "App0"
+},  {
+    "name": "Samsung Galaxy J8",
+    "company": "Samsung",
+    "img": "img/products/samsung-galaxy-j8-600x600-600x600.jpg",
+    "price": "6.290.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.0', HD+",
+        "os": "Android 8.0 (Oreo)",
+        "camara": "16 MP và 5 MP (2 camera)",
+        "camaraFront": "16 MP",
+        "cpu": "Qualcomm Snapdragon 450 8 nhân 64-bit",
+        "ram": "3 GB",
+        "rom": "32 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3500 mAh"
+    },
+    "masp": "Sam2"
+}, {
+    "name": "iPad 2018 Wifi 32GB",
+    "company": "Apple",
+    "img": "img/products/ipad-wifi-32gb-2018-thumb-600x600.jpg",
+    "price": "8.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "LED-backlit LCD, 9.7p''",
+        "os": "iOS 11.3",
+        "camara": "8 MP",
+        "camaraFront": "1.2 MP",
+        "cpu": "Apple A10 Fusion, 2.34 GHz",
+        "ram": "2 GB",
+        "rom": "32 GB",
+        "microUSB": "Không",
+        "battery": "Chưa có thông số cụ thể"
+    },
+    "masp": "App1"
+}, {
+    "name": "iPhone 7 Plus 32GB",
+    "company": "Apple",
+    "img": "img/products/iphone-7-plus-32gb-hh-600x600.jpg",
+    "price": "17.000.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "16.990.000"
+    },
+    "detail": {
+        "screen": "LED-backlit IPS LCD, 5.5', Retina HD",
+        "os": "iOS 11",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A10 Fusion 4 nhân 64-bit",
+        "ram": "3 GB",
+        "rom": "32 GB",
+        "microUSB": "Không",
+        "battery": "2900 mAh"
+    },
+    "masp": "App2"
+},{
+    "name": "Samsung Galaxy A7 (2018)",
+    "company": "Samsung",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/194327/samsung-galaxy-a7-2018-128gb-black-400x400.jpg",
+    "price": "8.990.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.0', Full HD+",
+        "os": "Android 8.0 (Oreo)",
+        "camara": "24 MP, 8 MP và 5 MP (3 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Exynos 7885 8 nhân 64-bit",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh"
+    },
+    "masp": "Sam3"
+},{
+    "name": "iPhone Xr 128GB",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/191483/iphone-xr-128gb-red-600x600.jpg",
+    "price": "24.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "22.990.000"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.1', IPS LCD, 16 triệu màu",
+        "os": "iOS 12",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A12 Bionic 6 nhân",
+        "ram": "3 GB",
+        "rom": "128 GB",
+        "microUSB": "Không",
+        "battery": "2942 mAh, có sạc nhanh"
+    },
+    "masp": "App3"
+}, {
+    "name": "iPhone 8 Plus 64GB",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/114110/iphone-8-plus-hh-600x600.jpg",
+    "price": "20.990.000",
+    "star": 5,
+    "rateCount": 230,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "LED-backlit IPS LCD, 5.5', Retina HD",
+        "os": "iOS 11",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic 6 nhân",
+        "ram": "3 GB",
+        "rom": "64 GB",
+        "microUSB": "Không",
+        "battery": "2691 mAh, có sạc nhanh"
+    },
+    "masp": "App4"
+}, {
+    "name": "iPhone 8 Plus 256GB",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/114114/iphone-8-plus-256gb-red-600x600.jpg",
+    "price": "25.790.000",
+    "star": 5,
+    "rateCount": 16,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "LED-backlit IPS LCD, 4.7', Retina HD",
+        "os": "iOS 11",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic 6 nhân",
+        "ram": "2 GB",
+        "rom": "256 GB",
+        "microUSB": "Không",
+        "battery": "1821 mAh, có sạc nhanh"
+    },
+    "masp": "App5"
+}, {
+    "name": "iPhone Xr 64GB",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/190325/iphone-xr-black-400x460.png",
+    "price": "22.990.000",
+    "star": 4,
+    "rateCount": 5,
+    "promo": {
+        "name": "giareonline",
+        "value": "19.990.000"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.1', IPS LCD, 16 triệu màu",
+        "os": "iOS 12",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A12 Bionic 6 nhân",
+        "ram": "3 GB",
+        "rom": "64 GB",
+        "battery": "2942 mAh, có sạc nhanh"
+    },
+    "masp": "App6"
+},]
